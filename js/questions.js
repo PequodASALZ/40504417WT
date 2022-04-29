e33a481659b2d7a48344d528c005c595fa69c45f
PequodASALZ <98317012+PequodASALZ@users.noreply.github.com>
2022-04-29 11:12:26 +0100

    Files for Quiz

---
+++ js/questions.js
@@ -0,0 +1,118 @@
+﻿// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What Spider-Man Actor Has A Tattoo Of Their Spider-Emblem And Where Is It On Their Body?",
+    answer: "Tom Holland, on the bottom of his foot",
+    options: [
+      "Andrew Garfield, on his buttocks",
+      "Tobey Maguire, on his arm",
+      "Tom Holland, on the bottom of his foot",
+      "None, No Spider-Man actor has gotten a tattoo"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is Spider-Man’s 6th Sense Called?",
+    answer: "Spidey-Sense",
+    options: [
+      "Spidey-Sense",
+      "Arachnid Awareness",
+      "Future Feeling",
+      "Tingles"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which Of These is NOT a Spider-Man Villain?",
+    answer: "Professor Pyg",
+    options: [
+      "Man-Wolf",
+      "Professor Pyg",
+      "The Spot",
+      "Bigwheel"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What Year Was The First Spider-Man Comic Released?",
+    answer: "1962",
+    options: [
+      "1972",
+      "1981",
+      "1962",
+      "1959"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which Of These is NOT One Of Spider-Man’s Love Interests?",
+    answer: "Selina Kyle",
+    options: [
+      "Selina Kyle",
+      "Mary Jane Watson",
+      "Felicia Hardy",
+      "Betty Brant"
+    ]
+  },
+
+     {
+     numb: 6,
+     question: "What Spider-Man Viillain Is In The Following Image?",
+     answer: "Venom",
+     options: [
+       "Carnage",
+       "Riot",
+       "Venom",
+       "Ant-Venom"
+     ]
+   },
+
+   {
+     numb: 7,
+     question: "Which Version Of Spider-Man is This?",
+     answer: "Superior Spider-Man",
+     options: [
+       "Ultimate Spider-Man",
+       "Savage Spider-Man",
+       "Superior Spider-Man",
+       "Scarlet Spider"
+     ]
+   },
+
+   {
+     numb: 8,
+     question: "Who Was The First Actor To Portray Spider-Man In Live Action?",
+     answer: "Danny Seagren",
+     options: [
+       "Tobey Maguire",
+       "Nicholas Hammond",
+       "Danny Seagren",
+       "Shinji Tōdō’s Takuya Yamashiro"
+     ]
+   },
+
+   {
+     numb: 9,
+     question: "What Newspaper Does Spider-Man work for?",
+     answer: "The Daily Bugle",
+     options: [
+       "The Daily Planet",
+       "The Daily Bugle",
+       "The Daily News",
+       "The Daily Globe"
+     ]
+   },
+
+   {
+     numb: 10,
+     question: "Stan Lee Was The Creator Of Spider-Man, But Who Else Created The Main Art For Spider-Man?",
+     answer: "Steve Ditko",
+     options: [
+       "Jared Lee",
+       "Michael Janssen",
+       "Steve Ditko",
+       "Edward Halton"
+     ]
+   },
+];
---
+++ js/script.js
@@ -0,0 +1,200 @@
+﻿//Refernce Code Video - learn-webdev
+//https://www.youtube.com/watch?v=MR8HyVQtNDk&list=PLFUJKIpMnHdcWmNEbL9QQnWDp9rhHCWXB&index=1&t=436s&ab_channel=learn-webdev
+
+//Refernce Code Video - Coding With Nepal
+//https://www.youtube.com/watch?v=pQr4O1OITJo&t=369s&ab_channel=CodingNepal
+
+//selecting all required elements
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+
+// Image Slider | learn-webdev
+var next = document.getElementById('NQ');
+
+var arr = ["img/1.jpeg", "img/2.jpeg", "img/3.jpeg", "img/4.jpeg", "img/5.jpeg", "img/6.jpeg", "img/7.jpeg", "img/8.jpeg", "img/9.jpeg", "img/10.jpeg" ];
+
+var imgcounter = 0;
+
+// if startQuiz button clicked
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo"); //show info box
+}
+
+// if exitQuiz button clicked
+exit_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide info box
+}
+
+// if continueQuiz button clicked
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.add("activeQuiz"); //show quiz box
+    showQuetions(0); //calling showQestions function
+    queCounter(1); //passing 1 parameter to queCounter
+
+    //Removing HTML elements when quiz starts
+    const element = document.getElementById('startimage')
+    element.remove();
+
+    const element2 = document.getElementById('subheader')
+    element2.remove();
+
+    const element3 = document.getElementById('startbut')
+    element3.remove();
+
+    const element4 = document.getElementById('startTitle')
+    element4.remove();
+}
+
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;
+
+//selecting all required elements
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+// if restartQuiz button clicked
+restart_quiz.onclick = ()=>{
+    quiz_box.classList.add("activeQuiz"); //show quiz box
+    result_box.classList.remove("activeResult"); //hide result box 
+    que_count = 0;
+    que_numb = 1;
+    userScore = 0;
+    widthValue = 0;
+    showQuetions(que_count); //calling showQestions function
+    queCounter(que_numb); //passing que_numb value to queCounter
+    clearInterval(counter); //clear counter
+    clearInterval(counterLine); //clear counterLine
+    next_btn.classList.remove("show"); //hide the next button
+}
+// if quitQuiz button clicked
+quit_quiz.onclick = ()=>{
+    window.location.reload(); //reload the current window
+}
+const next_btn = document.querySelector("footer .next_btn");
+const bottom_ques_counter = document.querySelector("footer .total_que");
+
+// if Next Que button clicked
+next_btn.onclick = ()=>{
+    imgcounter++; //increment imgcounter
+   if(imgcounter > arr.length - 1){
+        i = 0;
+   }
+     if(que_count < questions.length - 1){ //if question count is less than total question length
+        que_count++; //increment the que_count value
+        que_numb++; //increment the que_numb value
+        showQuetions(que_count); //calling showQestions function
+        queCounter(que_numb); //passing que_numb value to queCounter
+        clearInterval(counter); //clear counter
+        clearInterval(counterLine); //clear counterLine
+        next_btn.classList.remove("show"); //hide the next button
+        document.getElementById('image').src = arr[imgcounter];
+    }else{
+        clearInterval(counter); //clear counter
+        clearInterval(counterLine); //clear counterLine
+        showResult(); //calling showResult function
+    }
+}
+// getting questions and options from array
+function showQuetions(index){
+    const que_text = document.querySelector(".que_text");
+
+    //creating a new span and div tag for question and option and passing the value using array index
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    que_text.innerHTML = que_tag; //adding new span tag inside que_tag
+    option_list.innerHTML = option_tag; //adding new div tag inside option_tag
+    
+    const option = option_list.querySelectorAll(".option");
+
+    // set onclick attribute to all available options
+    for(i=0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+// creating the new div tags which for icons
+let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+//if user clicked on option
+function optionSelected(answer){
+    clearInterval(counter); //clear counter
+    clearInterval(counterLine); //clear counterLine
+    let userAns = answer.textContent; //getting user selected option
+    let correcAns = questions[que_count].answer; //getting correct answer from array
+    const allOptions = option_list.children.length; //getting all option items
+    
+    if(userAns == correcAns){ //if user selected option is equal to array's correct answer
+        userScore += 1; //upgrading score value with 1
+        answer.classList.add("correct"); //adding green color to correct selected option
+        answer.insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to correct selected option
+        console.log("Correct Answer");
+        console.log("Your correct answers = " + userScore);
+    }else{
+        answer.classList.add("incorrect"); //adding red color to correct selected option
+        answer.insertAdjacentHTML("beforeend", crossIconTag); //adding cross icon to correct selected option
+        console.log("Wrong Answer");
+
+        for(i=0; i < allOptions; i++){
+            if(option_list.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer 
+                option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
+                console.log("Auto selected correct answer.");
+            }
+        }
+    }
+    for(i=0; i < allOptions; i++){
+        option_list.children[i].classList.add("disabled"); //once a user has selected an option, all other options are disabled
+    }
+    next_btn.classList.add("show"); //show the next button if user selected any option
+}
+
+function showResult(){
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.remove("activeQuiz"); //hide quiz box
+    result_box.classList.add("activeResult"); //show result box
+    const scoreText = result_box.querySelector(".score_text");
+    
+    if (userScore == 10){ // if user scored 10
+        //creating a new span tag and passing the user score number and total question number
+        let scoreTag = '<span>Perfect Score! You Got <p>'+ userScore +'</p> Out Of <p> '+ questions.length +'</p> </span>';
+        scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text     
+    }
+    else if(userScore < 10 && userScore > 7){ // if user scored more than 1
+        let scoreTag = '<span>Great Score! You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else if(userScore == 5){ // if user scored 10
+        let scoreTag = '<span>Okay Score! You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else if(userScore == 0){ // if user scored 0
+        let scoreTag = '<span>Wow! Did You Even Try? You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else if(userScore < 5 && userScore > 1){ // if user scored 10
+        let scoreTag = '<span>Poor Score! You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else{ // if user scored less than 1
+        let scoreTag = '<span>Not Good! You only got <p>'+ userScore +'</p> out of <p> '+ questions.length +'</p> </span>';
+        scoreText.innerHTML = scoreTag;
+    }
+}
+function queCounter(index){
+    //creating a new span tag and passing the question number and total question
+    let totalQueCounTag = '<span><p>'+ index +'</p> of <p>'+ questions.length +'</p> Questions</span>';
+    bottom_ques_counter.innerHTML = totalQueCounTag;  //adding new span tag inside bottom_ques_counter
+}
